7d2566c278664fb057f7faeec1ab07e633151e86
Manav47699 <acharyamanav7@gmail.com>
2026-07-02 22:15:25 +0545

    updated

--- learning-react-native/src/app/index.tsx
+++ learning-react-native/src/app/index.tsx
@@ -1,17 +1,78 @@
-import { Button } from "expo-router/build/react-navigation";
-import { Text, View, StyleSheet, Pressable, TextInput } from "react-native";
-import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
+import { Text, View, StyleSheet, Pressable, Image } from "react-native";
+
 import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
+
 export default function Index() {
-  const { top} = useSafeAreaInsets();
+  // const { top} = useSafeAreaInsets();
   return (
-   < View style = {[styles.container, {paddingTop: top}]}>
+    <SafeAreaProvider> 
+      <SafeAreaView style = {styles.safeview}> 
+   {/* < View style = {[styles.container, {paddingTop: top}]}> */}
     {/* <Image source={}/> */}
 
    <Text>How are you feeling today?</Text>
-   
+   <View style= {{flexDirection: "row", justifyContent: "space-evenly"}}>
+   <Pressable style ={styles.button}>
+    <Image
+  source={require("../assets/images/very_happy.png")}
+  style={{ width: 40, height: 40 }}
+/>
+<Text>Very happy</Text>
+
+    
+
+   </Pressable>
+   <Pressable style ={styles.button}>
+    <Image
+  source={require("../assets/images/happy.png")}
+  style={{ width: 40, height: 40 }}
+/>
+<Text>Happy</Text>
+    
+
+   </Pressable>
+   {/* <Pressable style ={styles.button}>
+    <Image
+  source={require("../assets/images/very_happy.png")}
+  style={{ width: 40, height: 40 }}
+/>
+Very happy
+    
+
+   </Pressable> */}
+   <Pressable style ={styles.button}>
+    <Image
+  source={require("../assets/images/neutral.png")}
+  style={{ width: 40, height: 40 }}
+/>
+<Text>Neutral</Text>
+    
+
+   </Pressable>
+   <Pressable style ={styles.button}>
+    <Image
+  source={require("../assets/images/sad.png")}
+  style={{ width: 40, height: 40 }}
+/>
+<Text>Sad</Text>
+    
+
+   </Pressable>
+   <Pressable style ={styles.button}>
+    <Image
+  source={require("../assets/images/stressed.png")}
+  style={{ width: 40, height: 40 }}
+/>
+<Text>Stressed</Text>
+    
+
+   </Pressable>
    </View>
+
+   {/* </View> */}
+   </SafeAreaView>  
+   </SafeAreaProvider>
     
     
     
@@ -24,6 +85,18 @@ const styles = StyleSheet.create({
     backgroundColor: "whites",
     flex: 1,
 
+  },
+  safeview:{
+    gap: 30
+    
+  },
+  button:{
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "gray",
+    alignItems: "center",
+    // flexWrap: "wrap",
+    padding: 10
   }
 
 })
